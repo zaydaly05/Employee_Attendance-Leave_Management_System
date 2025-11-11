0fe8d4d036867a6afc3b71b4ae07d21d6c75adf5
Zayd <zaydaly05@gmail.com>
2025-11-11 13:50:56 +0200

    dtbse

---
+++ Models/dbConnect.php
@@ -0,0 +1,17 @@
+
+<?php
+class Database {
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $dbname = "attendance_system";
+    public $conn;
+
+    public function __construct() {
+        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+        if ($this->conn->connect_error) {
+            die("❌ Database connection failed: " . $this->conn->connect_error);
+        }
+    }
+}
+?>
